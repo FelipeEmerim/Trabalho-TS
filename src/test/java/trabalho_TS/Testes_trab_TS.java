306bf4d585b5624d6a14379898dcab3f9882249d
FelipeEmerim <lip.emerim@gmail.com>
2017-11-09 10:26:43 -0200

    Commit resgatado

---
+++ src/test/java/trabalho_TS/Testes_trab_TS.java
@@ -0,0 +1,11 @@
+package trabalho_TS;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UsuarioTest.class, VisitaTest.class })
+public class Testes_trab_TS {
+
+}
